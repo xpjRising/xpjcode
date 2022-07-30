bc4433c3829757747a9ac48fae03e706524ea985
xpjRising <2560835320@qq.com>
2022-07-30 17:39:12 +0800

    first commit

---
+++ daliy practice/练习/练习.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    int m, i, n = 0;
+    for (m = 100; m <= 200; m = m + 1)
+    {
+        for (i = 2; i * i <= m; i++)
+            if (m % i == 0)
+            {
+                break;
+            }
+        if (m % i != 0)
+        {
+            cout << m << " ";
+            n++;
+        }
+    }
+    cout << endl;
+    cout << n << endl;
+    return 0;
+}
